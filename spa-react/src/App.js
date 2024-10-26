8d24fae59ae54b24228dc4050d749603ede457cc
KyawZayYa <kyawzayya100417@gmail.com>
2024-10-26 13:32:13 +0630

    conditional rendering

--- spa-react/src/App.js
+++ spa-react/src/App.js
@@ -12,18 +12,25 @@ export default function App() {
     SetPosts((prevPost => posts.filter((post) => post.id !== id)));
   }
 
+  // && and gate concept base 
+  // true && true = true 
+  // true && false = false 
+  // true && console.log('hello world') = 'hello world' 
+
   return (
     <div>
       <h1>Posts</h1>
       <ul>
         {
-          posts.map((post) => (
+          !!posts.length && posts.map((post) => (
             <li key={post.id}>
               {post.title}
             <button onClick={() => deletePost(post.id)}>Delete</button>
             </li>
           ))
         }
+
+        {!posts.length && <p>No post available.</p> }
       </ul>
     </div>
   )
